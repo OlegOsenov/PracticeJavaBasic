0a36b9c7b28e990adfab642b7c8af61c5a231404
Oleg Osenov <olejka_oon@mail.ru>
2023-01-03 15:47:14 +0500

    Фиббоначи

--- src/main/java/easyCodeRuPractice/Lesson14.java
+++ src/main/java/easyCodeRuPractice/Lesson14.java
@@ -6,6 +6,23 @@ public class Lesson14 {
         showNumbersDividedBy3(10);
         print("Задание 2: ");
         showNumbersDividedByArg(10, 4);
+        print("Задание 3: ");
+        showFibbonachi(10);
+    }
+
+    private static void showFibbonachi(int max) {
+        int item0 = 1;
+        int item1 = 1;
+        for (int i = 0; i < max; i++) {
+            if (i == 0 || i == 1){
+                print(1);
+            }else {
+                print(item0 + item1);
+                int item1Old = item1;
+                item1 = item0 + item1;
+                item0 = item1Old;
+            }
+        }
     }
 
     private static void showNumbersDividedByArg(int max, int arg) {
