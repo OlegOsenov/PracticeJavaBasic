d94bc328545be6e6ec7daaedbbb735a247bb91e7
Oleg Osenov <olejka_oon@mail.ru>
2023-01-05 21:59:38 +0500

    Задание 15.1 исправил equals

--- src/main/java/easyCodeRu/lesson15/Lesson.java
+++ src/main/java/easyCodeRu/lesson15/Lesson.java
@@ -1,7 +1,9 @@
 package easyCodeRu.lesson15;
 
 import java.util.Scanner;
-
+/* Написать программу, которая будет приветствовать всех, кроме Даниила. т.е. вы вводите
+имя, программа выдает “Привет, “ + введенное имя. До тех пор пока вы не ввели имя Даниил.
+  */
 public class Lesson {
     static Scanner scanner = new Scanner(System.in);
     static String nameDan = "Даниил";
@@ -11,12 +13,13 @@ public class Lesson {
             print("Введите имя: ");
             String nameScan = scanner.nextLine();
 
-            if (nameScan.equals(nameDan)){
+            if (nameDan.equals(nameScan)){
                 break;
             }
             print("Вы ввели имя : " + nameScan);
         }
         print("Программа завершена");
+        scanner.close();
     }
     public static void print(String str){
         System.out.println(str);
