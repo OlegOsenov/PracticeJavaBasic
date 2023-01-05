0bd48302e229fc710d0b7af55aef6476ac8b8346
Oleg Osenov <olejka_oon@mail.ru>
2023-01-05 22:05:21 +0500

    Задание 15.2 не большая доработка

--- src/main/java/easyCodeRu/lesson15/UserScanFive.java
+++ src/main/java/easyCodeRu/lesson15/UserScanFive.java
@@ -3,15 +3,21 @@ package easyCodeRu.lesson15;
 import java.util.Scanner;
 
 public class UserScanFive {
+    /* 15.2   Для каждого введенного в консоль числа проверять кратно ли оно 5 или нет. Если
+кратно,то выводить в консоль – кратное 5. Ограничить ввод 20 числами. (условие) и
+фигурные скобки. Пока условие равно оператор %) */
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         int count = 1;
         while (count <= 20) {
+            System.out.println("Введите ваше число: ");
             count++;
             int number = scanner.nextInt();
             if (number % 5 == 0) {
                 System.out.println("Кратное 5");
+            } else{
+                System.out.println("Не кратно 5");
             }
         }
     }
