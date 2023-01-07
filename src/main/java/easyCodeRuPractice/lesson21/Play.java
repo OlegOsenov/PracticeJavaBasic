8115e7893d429edb7d3036fed2cf657f1ad4cac2
Oleg Osenov <olejka_oon@mail.ru>
2023-01-07 16:42:02 +0500

    21 лекция добавил объекты классов

---
+++ src/main/java/easyCodeRuPractice/lesson21/Play.java
@@ -0,0 +1,18 @@
+package easyCodeRuPractice.lesson21;
+
+public class Play {
+    public static void main(String[] args) {
+        Fiqure[] fiqures = new Fiqure[3];
+        fiqures[0] = new Circle(2);
+        fiqures[1] = new Rectangle(5, 6);
+        fiqures[2] = new Triangle(3, 4, 5);
+
+        for (Fiqure figure: fiqures) {
+            print("area: " + figure.getArea() + ", perimetr: " + figure.getPerimetr());
+        }
+    }
+
+    private static void print(String str){
+        System.out.println(str);
+    }
+}
