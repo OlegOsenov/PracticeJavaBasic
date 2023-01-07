05431bff27b1a711769464e59a1f34e756c0daaa
Oleg Osenov <olejka_oon@mail.ru>
2023-01-07 20:38:55 +0500

    18 лекция, команды с помощью класса

--- src/main/java/easyCodeRuPractice/lesson18/CommandAndResult.java
+++ src/main/java/easyCodeRuPractice/lesson18/CommandAndResult.java
@@ -2,6 +2,12 @@ package easyCodeRuPractice.lesson18;
 
 public class CommandAndResult {
 
-    String command;
-    String result;
+    final String command;
+    final String result;
+
+    public CommandAndResult(String command, String result) {
+        this.command = command;
+        this.result = result;
+        System.out.println("command" + this.command + ", result: " + this.result);
+    }
 }
--- src/main/java/easyCodeRuPractice/lesson18/Main.java
+++ src/main/java/easyCodeRuPractice/lesson18/Main.java
@@ -1,21 +1,41 @@
 package easyCodeRuPractice.lesson18;
 
-public class Main {
-    public static void main(String[] args) {
-        CommandAndResult commandAndResult = new CommandAndResult();
-        commandAndResult.command = "my command";
-        commandAndResult.result = "my result";
-        System.out.println(commandAndResult.command);
-        System.out.println(commandAndResult.result);
-        /*
-        COMMAND_AND_RESULTS[0].command = "/help";
-        COMMAND_AND_RESULTS[0].result = "";
-        COMMAND_AND_RESULTS[1].command = "/start";
-        COMMAND_AND_RESULTS[1].result = "Bot started";
+import java.util.Scanner;
 
-         */
-    }
+public class Main {
     private static final CommandAndResult[] COMMAND_AND_RESULTS = new CommandAndResult[]{
-            new CommandAndResult(),new CommandAndResult(),new CommandAndResult()
+            new CommandAndResult("/help",""),
+            new CommandAndResult("/start","Bot started"),
+            new CommandAndResult("/getLocation","location is Moscow"),
+            new CommandAndResult("/end","Bot ended")
     };
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String commands = "commands available for this bot \n";
+        for (CommandAndResult item:COMMAND_AND_RESULTS) {
+            commands += item.command + "\n";
+        }
+        COMMAND_AND_RESULTS[0] = new CommandAndResult(COMMAND_AND_RESULTS[0].command,commands);
+
+        String input;
+        boolean found;
+        while (true){
+            input = scanner.nextLine();
+            found = false;
+            for (int i = 0; i < COMMAND_AND_RESULTS.length; i++) {
+                if (COMMAND_AND_RESULTS[i].command.equals(input)){
+                    System.out.println(COMMAND_AND_RESULTS[i].result);
+                    found = true;
+                    break;
+                }
+            }
+            if (COMMAND_AND_RESULTS[COMMAND_AND_RESULTS.length - 1].command.equals(input)) {
+                break;
+            }
+            if (!found){
+                System.out.println("no command found for that input.Try typing " + COMMAND_AND_RESULTS[0].command);
+            }
+        }
+    }
+
 }
