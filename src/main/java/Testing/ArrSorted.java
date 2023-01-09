e11eda35a6160781442be5c593b07fd6922a8a12
Oleg Osenov <olejka_oon@mail.ru>
2023-01-09 22:02:32 +0500

    Сортировка массива

---
+++ src/main/java/Testing/ArrSorted.java
@@ -0,0 +1,15 @@
+package Testing;
+
+public class ArrSorted {
+    public static void main(String[] args) {
+
+        int[] array = new int[]{2,1,4,3,5,6,9,8,7,10};
+
+    }
+
+    private static int minSorted(int[] array, int start){
+        int minIndex = start;
+        return minIndex;
+    }
+
+}
