dba3916d9d5874cfa13d0265d1b75f93536b74da
Oleg Osenov <olejka_oon@mail.ru>
2023-01-10 20:25:36 +0500

    Лекция 22, метод для порождения объекта круг,треугольник, прямоугольник

--- src/main/java/easyCodeRuPractice/lesson22/FigureFactory.java
+++ src/main/java/easyCodeRuPractice/lesson22/FigureFactory.java
@@ -1,4 +1,21 @@
 package easyCodeRuPractice.lesson22;
 
+import easyCodeRuPractice.lesson21.Circle;
+import easyCodeRuPractice.lesson21.Fiqure;
+import easyCodeRuPractice.lesson21.Rectangle;
+import easyCodeRuPractice.lesson21.Triangle;
+
 public class FigureFactory {
+
+    public Fiqure create(double[] sides){
+        if (sides.length == 1){
+            return new Circle(sides[0]);
+        } else if (sides.length == 2) {
+            return new Rectangle(sides[0], sides[1]);
+        } else if (sides.length == 3) {
+            return new Triangle(sides[0],sides[1],sides[3]);
+        } else {
+            throw new IllegalArgumentException("can't create object with arguments " + sides);
+        }
+    }
 }
