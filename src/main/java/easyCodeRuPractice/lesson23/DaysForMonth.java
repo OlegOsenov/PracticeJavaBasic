be8b013918e46badeaf1bd609dd63dcf0bcba3e8
Oleg Osenov <olejka_oon@mail.ru>
2023-01-11 19:24:29 +0500

    Лекция 23 отдаем количество дней в месяце по строковому параметру, добавил case на кириллице

--- src/main/java/easyCodeRuPractice/lesson23/DaysForMonth.java
+++ src/main/java/easyCodeRuPractice/lesson23/DaysForMonth.java
@@ -4,19 +4,25 @@ public class DaysForMonth {
     public static void main(String[] args) {
         print(getDaysForMonth("JANUARY"));
         print(getDaysForMonth("january"));
+        print(getDaysForMonth("февраль"));
 
 
     }
 
-    private static int getDaysForMonth(String month){
-        switch (month.toLowerCase()){
-            case "january" : return  31;
-            case "february" : return  28;
-            default : throw new IllegalArgumentException("not recognized month " + month);
+    private static int getDaysForMonth(String month) {
+        switch (month.toLowerCase()) {
+            case "january":
+            case "январь":
+                return 31;
+            case "february":
+            case "февраль":
+                return 28;
+            default:
+                throw new IllegalArgumentException("not recognized month " + month);
         }
     }
 
-    private static void print(int str){
+    private static void print(int str) {
         System.out.println(str);
     }
 }
