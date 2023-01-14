58cf6bad2033aa0b5ce8045f2e65baf2ae934832
Oleg Osenov <olejka_oon@mail.ru>
2023-01-14 19:37:17 +0500

    Начало, нахождения мин значения в массиве

--- src/main/java/Testing/SordetArray/ArrSorted.java
+++ src/main/java/Testing/SordetArray/ArrSorted.java
@@ -3,5 +3,21 @@ package Testing.SordetArray;
 public class ArrSorted {
     public static void main(String[] args) {
 
+        int[] array = new int[]{3, 4, 1, 6, 9, 5, 6, 4};
+
+
+    }
+
+    private static int min(int[] array){
+        int minIndex = 0;
+        int minValue = array[0];
+        for (int q = 0; q < array.length; q++) {
+            if (array[q] < minValue){
+                minValue = array[q];
+                minIndex = q;
+            }
+
+        }
+        return minIndex;
     }
 }
