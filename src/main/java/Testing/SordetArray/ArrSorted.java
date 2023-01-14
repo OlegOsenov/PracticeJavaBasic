21d48664707308280236072a2d0768dc230dc13b
Oleg Osenov <olejka_oon@mail.ru>
2023-01-14 21:01:34 +0500

    Начало, сортировка массива

--- src/main/java/Testing/SordetArray/ArrSorted.java
+++ src/main/java/Testing/SordetArray/ArrSorted.java
@@ -1,18 +1,29 @@
 package Testing.SordetArray;
 
+import java.util.Arrays;
+
 public class ArrSorted {
     public static void main(String[] args) {
 
         int[] array = new int[]{3, 4, 1, 6, 9, 5, 6, 4};
 
+        for (int stepa = 0; stepa < array.length; stepa++) {
+            int index = min(array,stepa);
+            int tmp = array[stepa];
+            array[stepa] = array[index];
+            array[index] = tmp;
+        }
+
+        System.out.println(Arrays.toString(array));
+
 
     }
 
-    private static int min(int[] array){
-        int minIndex = 0;
-        int minValue = array[0];
-        for (int q = 0; q < array.length; q++) {
-            if (array[q] < minValue){
+    private static int min(int[] array, int start) {
+        int minIndex = start;
+        int minValue = array[start];
+        for (int q = start + 1; q < array.length; q++) {
+            if (array[q] < minValue) {
                 minValue = array[q];
                 minIndex = q;
             }
