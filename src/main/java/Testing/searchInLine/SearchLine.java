198b1d55a3a85fe3e5778660996e91de2b3ff89f
Oleg Osenov <olejka_oon@mail.ru>
2023-01-16 21:54:19 +0500

    Сделал алгоритм для нахождения совпадений

--- src/main/java/Testing/searchInLine/SearchLine.java
+++ src/main/java/Testing/searchInLine/SearchLine.java
@@ -1,5 +1,8 @@
 package Testing.searchInLine;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class SearchLine {
 
     public static void main(String[] args) {
@@ -7,7 +10,22 @@ public class SearchLine {
         String text = "aabaabaaaaabaaaa";
         String sample = "aabaa";
 
+        System.out.println(Arrays.toString(searchNaive(text,sample).toArray()));
+
     }
 
+    static ArrayList<Integer> searchNaive(String text, String sample){
+        ArrayList<Integer> foundPositions = new ArrayList<>();
+        for (int i = 0; i < text.length(); i++) {
+            int j = 0;
+            while (j < sample.length() && i + j < text.length() && sample.charAt(j) == text.charAt(i+j)){
+                j++;
+            }
+            if (j == sample.length()){
+                foundPositions.add(i);
+            }
+        }
+        return foundPositions;
+    }
 
 }
