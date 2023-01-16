77f421a68b06fc1ab8bdd58c117c2214ccb7e774
Oleg Osenov <olejka_oon@mail.ru>
2023-01-16 22:33:09 +0500

    Сделал алгоритм для вычисления префиксной функции

--- src/main/java/Testing/searchInLine/SearchLine.java
+++ src/main/java/Testing/searchInLine/SearchLine.java
@@ -10,22 +10,35 @@ public class SearchLine {
         String text = "aabaabaaaaabaaaa";
         String sample = "aabaa";
 
-        System.out.println(Arrays.toString(searchNaive(text,sample).toArray()));
+        System.out.println(Arrays.toString(searchNaive(text, sample).toArray()));
+        System.out.println(Arrays.toString(prefixFunction(sample)));
 
     }
 
-    static ArrayList<Integer> searchNaive(String text, String sample){
+    static ArrayList<Integer> searchNaive(String text, String sample) {
         ArrayList<Integer> foundPositions = new ArrayList<>();
-        for (int i = 0; i < text.length(); i++) {
+        for (int i = 1; i < text.length(); i++) {
             int j = 0;
-            while (j < sample.length() && i + j < text.length() && sample.charAt(j) == text.charAt(i+j)){
+            while (j < sample.length() && i + j < text.length() && sample.charAt(j) == text.charAt(i + j)) {
                 j++;
             }
-            if (j == sample.length()){
+            if (j == sample.length()) {
                 foundPositions.add(i);
             }
         }
         return foundPositions;
+    }
+
+    static int[] prefixFunction(String sample) {
+        int[] values = new int[sample.length()];
+        for (int i = 0; i < sample.length(); i++) {
+            int j = 0;
+            while (i + j < sample.length() && sample.charAt(j) == sample.charAt(i + j)) {
+                values[i + j] = Math.max(values[i + j], j + 1);
+                j++;
+            }
+        }
+        return values;
     }
 
 }
