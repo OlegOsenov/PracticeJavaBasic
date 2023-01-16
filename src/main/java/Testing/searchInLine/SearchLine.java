7a1537e2fb170baa76db214e03b70c4634d71b7f
Oleg Osenov <olejka_oon@mail.ru>
2023-01-16 21:44:26 +0500

    Начало, создал две переменные String для последующего сравнения

--- src/main/java/Testing/searchInLine/SearchLine.java
+++ src/main/java/Testing/searchInLine/SearchLine.java
@@ -1,4 +1,13 @@
 package Testing.searchInLine;
 
 public class SearchLine {
+
+    public static void main(String[] args) {
+
+        String text = "aabaabaaaaabaaaa";
+        String sample = "aabaa";
+
+    }
+
+
 }
