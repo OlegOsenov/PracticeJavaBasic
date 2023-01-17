d9eeffde67bfd9d1ce89055fe9701a09a23dd02f
Oleg Osenov <olejka_oon@mail.ru>
2023-01-17 21:10:58 +0300

    Поиск файлов на диске

---
+++ src/main/java/Testing/searchFile/Main.java
@@ -0,0 +1,36 @@
+package Testing.searchFile;
+
+import javax.annotation.processing.Filer;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<File> fileList = new ArrayList<>();
+        searchFiles(new File("C:\\"), fileList);
+        for (File file: fileList) {
+            System.out.println(file.getAbsolutePath());
+        }
+
+    }
+
+    private static void searchFiles(File rootFile, List<File> fileList) {
+        if (rootFile.isDirectory()) {
+            System.out.println("searching at: " + rootFile.getAbsolutePath());
+            File[] directoryFiles = rootFile.listFiles();
+            if (directoryFiles != null) {
+                for (File file : directoryFiles) {
+                    if (file.isDirectory()) {
+                        searchFiles(file, fileList);
+                    } else {
+                        if (file.getName().toLowerCase().endsWith(".jpq")){
+                            fileList.add(file);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
